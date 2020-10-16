d3722d97934203cec33ef1d4269b11c7408e02fd
Tomek Garbarz <tomasz.garbarz91@gmail.com>
2020-10-16 11:49:34 +0200

    Product init

--- js/app.js
+++ js/app.js
@@ -14,3 +14,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
