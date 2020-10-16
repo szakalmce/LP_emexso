51eaa16e75044ae83130f4628d90ead5e324ad12
Tomek Garbarz <tomasz.garbarz91@gmail.com>
2020-10-16 16:56:11 +0200

    Product end

--- js/app.js
+++ js/app.js
@@ -1,34 +1,39 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav: false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
+$(document).ready(function () {
+    $('.owl-one').owlCarousel({
+        loop:true,
+        margin:10,
+        nav: false,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
         }
-    }
+    })
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:30,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        navText: ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
 })
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav: true,
-    dots: false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
-    }
-})
